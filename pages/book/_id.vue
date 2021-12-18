ff2ffb1524d25924b78e0eeee40717aaafd757ca
Fabrício S. Batalha <fabriciobatalha.dev@gmail.com>
2021-12-18 17:42:55 -0400

    feat: detalhes em geral e página de detalhes do e-book

--- pages/book/_id.vue
+++ pages/book/_id.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <h1> {{ $route.params.id }} </h1>
-  </div>
+  <BookDetailsTemplate />
 </template>
 
 <script lang="ts">
@@ -9,6 +7,7 @@ import Vue from 'vue'
 import { books } from '@/store'
 
 export default Vue.extend({
+  layout: 'ppv',
   async asyncData({ params }) {
     await books.show({ id: params.id as any })
   }
